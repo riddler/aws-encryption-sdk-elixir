08f778c86b1ea17cf63788ffa36d0cfe4c9c2ebc
JohnnyT <ubergeek3141@gmail.com>
2026-01-24 15:28:37 -0700

    Adds ExQuality along with other code quality tools
    Closes #2

--- mix.exs
+++ mix.exs
@@ -7,7 +7,14 @@ defmodule AwsEncryptionSdk.MixProject do
       version: "0.1.0",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
 
@@ -21,8 +28,12 @@ defmodule AwsEncryptionSdk.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:dialyxir, "~> 1.4", only: [:dev], runtime: false},
+      {:excoveralls, "~> 0.18.5", only: :test},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:doctor, "~> 0.22.0", only: :dev},
+      {:mix_audit, "~> 2.1", only: [:dev, :test], runtime: false},
+      {:ex_quality, "~> 0.2.0", only: [:dev, :test]}
     ]
   end
 end
